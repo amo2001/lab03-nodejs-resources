ac6d006d2f996fa2ef411fa20e92de5fb6fa6ef5
amo2001 <amo2001@hw.ac.uk>
2021-11-22 10:26:36 +0400

    hi

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 //send the index.html when receiving HTTP GET /
 app.use(express.static('public'));
 app.get('/', (req, res) => {
-    res.sendFile('public/index.html', { root: __dirname });
+    res.sendFile('index.html', { root: __dirname });
    });
 //make the app listen on port 
 const port = process.argv[2] || process.env.PORT || 3000;
@@ -17,3 +17,8 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
  res.render('index'); //no need for ejs extension
 });
+//route for contacts
+app.get('/contacts', (req, res) => {
+    res.render('contacts'); 
+   });
+   
